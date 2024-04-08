c6dc33ee541d2211df3c974cf9cb94b79e63af72
Aarne Harju <aarne.harju@sogeti.com>
2024-04-08 19:01:51 +0300

    React: Button now turns grey when disabled
    Button now turns grey when disabled. Also refactored nextColor variable to use useState.

--- color-button/src/App.jsx
+++ color-button/src/App.jsx
@@ -5,15 +5,29 @@ import "./App.css";
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
   const [checked, setChecked] = useState(false);
-  const nextColor = buttonColor === "red" ? "blue" : "red";
-  
+  // const nextColor = buttonColor === "red" ? "blue" : "red";
+  const [nextColor, setNextColor] = useState("blue");
+
+  const handleClick = () => {
+    setButtonColor(nextColor);
+    setNextColor(nextColor === "blue" ? "red" : "blue");
+  }
+
   const handleCheckbox = () => {
+    const colorBeforeDisabling = nextColor === "red" ? "blue" : "red";
+    
+    if (!checked) {
+      setButtonColor("gray");
+    } else {
+      setButtonColor(colorBeforeDisabling);
+    }
     setChecked(!checked);
+    
   }
 
   return (
     <div id="container">
-      <button className={buttonColor} onClick={() => setButtonColor(nextColor)} disabled={checked} >Change to {nextColor}!</button>
+      <button className={buttonColor} onClick={() => handleClick()} disabled={checked} >Change to {nextColor}!</button>
       <div id="disable-button-group">
         <label htmlFor="checkbox-disable-button">Disable button</label>
         <input id="checkbox-disable-button" type="checkbox" value={checked} onChange={handleCheckbox}/>
