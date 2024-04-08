9b9947a97a7d64a94723bd1a80919aa7c8b4ce2d
Aarne Harju <aarne.harju@sogeti.com>
2024-04-08 09:59:02 +0300

    Added first test

--- color-button/src/App.jsx
+++ color-button/src/App.jsx
@@ -1,6 +1,11 @@
+import "./App.css";
+
+
 function App() {
   return (
-    <div></div>
+    <div>
+      <button className="red">Change to blue!</button>
+    </div>
   );
 }
 
--- color-button/src/App.test.jsx
+++ color-button/src/App.test.jsx
@@ -1,5 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+test("Button starts with correct color", () => {
+    render(<App/>);
+    const elementButton = screen.getByRole("button", { name: /blue/i });
+    expect(elementButton).toHaveClass("red");
+});
 
+test("Button starts with correct text", () => {
+
+});
+
+test("Button has correct color after click", () => {
+
+});
+
+test("Button has correct text after click", () => {
+
+});
 
