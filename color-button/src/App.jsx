cce900355ee65585e28eb55332d9c8c786dba4b3
Aarne Harju <aarne.harju@sogeti.com>
2024-04-08 17:01:25 +0300

    Created react code for the checkbox

--- color-button/src/App.jsx
+++ color-button/src/App.jsx
@@ -4,12 +4,20 @@ import "./App.css";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [checked, setChecked] = useState(false);
   const nextColor = buttonColor === "red" ? "blue" : "red";
-  const [buttonColorText, setButtonColorText] = useState("blue");
+  
+  const handleCheckbox = () => {
+    setChecked(!checked);
+  }
 
   return (
-    <div>
-      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>Change to {nextColor}!</button>
+    <div id="container">
+      <button className={buttonColor} onClick={() => setButtonColor(nextColor)} disabled={checked} >Change to {nextColor}!</button>
+      <div id="disable-button-group">
+        <label htmlFor="checkbox-disable-button">Disable button</label>
+        <input id="checkbox-disable-button" type="checkbox" value={checked} onChange={handleCheckbox}/>
+      </div>
     </div>
   );
 }
