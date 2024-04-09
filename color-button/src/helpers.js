4612cabcfbb4de58d5a1f3fd6f5e1b2cddecb051
Aarne Harju <aarne.harju@sogeti.com>
2024-04-09 11:39:48 +0300

    Created the kebabCaseToTitleCase() helper function - green

--- color-button/src/helpers.js
+++ color-button/src/helpers.js
@@ -1,1 +1,5 @@
-export function kebabCaseToTitleCase() {};
+export function kebabCaseToTitleCase(inputText) {
+    const inputWithDashesConvertedToSpaces = inputText.replaceAll("-", " ");
+    const inputConvertedToTitleCase = inputWithDashesConvertedToSpaces.replace(/\b([a-z])/g, (match) => match.toUpperCase());
+    return inputConvertedToTitleCase;
+};
