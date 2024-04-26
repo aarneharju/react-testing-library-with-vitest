a786221456845434e0a043dd36938e764adc90ba
Aarne Harju <aarne.harju@sogeti.com>
2024-04-26 09:29:33 +0300

    Options component now renders options

--- sundae-front-end/src/pages/entry/Options.jsx
+++ sundae-front-end/src/pages/entry/Options.jsx
@@ -4,10 +4,14 @@ import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function Options({ optionType }) {
     const [items, setItems] = useState([]);
     const [error, setError] = useState(false);
+    const { totals } = useOrderDetails();
 
     // Option type is 'scoops' or 'toppings'
     useEffect(() => {
@@ -24,12 +28,19 @@ export default function Options({ optionType }) {
         return <AlertBanner />
     }
 
-    // TODO: Replace 'null' with ToppingOption when available
     const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+    const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
     
     const optionItems = items.map(item => (
         <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
     ));
 
-    return <Row>{optionItems}</Row>;
+    return (
+        <>
+            <h2>{ title }</h2>
+            <p>{ formatCurrency(pricePerItem[optionType]) } each</p>
+            <p>{ title } total: {formatCurrency(totals[optionType])}</p>
+            <Row>{optionItems}</Row>;
+        </>
+    )
 }
