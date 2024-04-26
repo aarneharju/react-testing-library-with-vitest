cdb128fc53bedfc073feaaf29d6c9bd54cacc0f1
Aarne Harju <aarne.harju@sogeti.com>
2024-04-26 09:34:28 +0300

    Updated OrderSummary component to actually render something

---
+++ sundae-front-end/src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import SummaryForm from "./SummaryForm";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
+
+export default function OrderSummary() {
+    const { totals, optionCounts } = useOrderDetails();
+    const scoopArray = Object.entries(optionCounts.scoops); // [["Chocolate", 2], ["Vanilla", 1]]
+    const scoopList = scoopArray.map(([key, value]) => (
+        <li key={ key } >
+            { value } { key }
+        </li>
+    ));
+    const toppingsArray = Object.entries(optionCounts.toppings); // ["M&Ms", "Gummi bears"]
+    const toppingList = toppingsArray.map(topping => <li key={ topping } >{ topping }</li> )
+
+    return (
+        <div>
+            <h1>Order Summary</h1>
+            <h2>Scoops: { formatCurrency(totals.scoops) }</h2>
+            <ul>
+               { scoopList }
+            </ul>
+            <h2>Toppings:</h2>
+            <ul>
+                { toppingList }
+            </ul>
+            <h2>Toppings: { formatCurrency(totals.toppings) }</h2>
+            <SummaryForm />
+        </div>
+    )
+}
