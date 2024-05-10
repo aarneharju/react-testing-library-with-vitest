4af6188509eb52be4fb874ac9cb6956d8372dc5a
Aarne Harju <aarne.harju@sogeti.com>
2024-05-10 15:53:13 +0300

    Added grand total to OrderEntry component

--- sundae-front-end/src/pages/entry/OrderEntry.jsx
+++ sundae-front-end/src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,21 @@
 import Options from "./Options";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
 
 export default function OrderEntry() {
+    const { totals } = useOrderDetails();
+
     return (
-        <div>
-            <Options optionType="scoops" />
-            <Options optionType="toppings" />
-        </div>
+        <>
+            <div>
+                <Options optionType="scoops" />
+                <Options optionType="toppings" />
+            </div>
+            <div>
+                <h2>
+                    Grand Total: { formatCurrency(totals.scoops + totals.toppings) }
+                </h2>
+            </div>
+        </>
     );
 }
