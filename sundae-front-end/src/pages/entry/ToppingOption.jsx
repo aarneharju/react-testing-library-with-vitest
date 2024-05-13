d4e8836e7d6dcc659a798039a11705a573424ca5
Aarne Harju <aarne.harju@sogeti.com>
2024-05-13 14:50:45 +0300

    Changed the rendering sizes of toppings

--- sundae-front-end/src/pages/entry/ToppingOption.jsx
+++ sundae-front-end/src/pages/entry/ToppingOption.jsx
@@ -7,7 +7,7 @@ export default function ToppingOption({ name, imagePath }) {
     const handleChange = (event) => updateItemCount(name, event.target.checked ? 1 : 0, "toppings");
 
     return (
-        <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+        <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
             <img style={{ width: "75%"}} src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
             <Form.Group controlId={`${ name }-topping-checkbox`}>
                 <Form.Check
