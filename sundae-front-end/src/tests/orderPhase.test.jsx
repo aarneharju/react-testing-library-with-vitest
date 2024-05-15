fcdcc75f643fff214117da56d19f515428d53d24
Aarne Harju <aarne.harju@sogeti.com>
2024-05-15 10:07:36 +0300

    Added skeleton for orderPhase tests

---
+++ sundae-front-end/src/tests/orderPhase.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App.jsx"
+
+test("Order phases for happy path", () => {
+    // Render app
+
+    // Add a scoop and a topping
+
+    // Find and click order button
+
+    // Check summary information based on order
+
+    // Accept terms and conditions and click button to confirm order
+
+    // Confirm order number on confirmation page
+
+    // Click new order button on confirmation page to go back to beginning
+
+    // Check that scoops and toppings subtotals have been reset
+})
