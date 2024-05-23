cdad4ed142e904d9190e5383ce10e7f76d473027
Aarne Harju <aarne.harju@sogeti.com>
2024-05-23 14:44:28 +0300

    Style: Added whitespace

--- sundae-front-end/src/pages/confirmation/OrderConfirmation.jsx
+++ sundae-front-end/src/pages/confirmation/OrderConfirmation.jsx
@@ -28,7 +28,7 @@ export default function OrderConfirmation(props) {
     return (
         <div>
             <h2>Thank you!</h2>
-            <h3>Your order number is {orderNumber ? orderNumber : "loading..."}</h3>
+            <h3>Your order number is: { orderNumber ? orderNumber : "Loading..." }</h3>
             <p>As per our terms and conditions, nothing will happen now</p>
             <Button onClick={ handleClick }>Create new order</Button>
         </div>
