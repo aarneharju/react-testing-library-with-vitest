e89f678f960581f73c35503054174d30d0c36c8b
Aarne Harju <aarne.harju@sogeti.com>
2024-05-24 12:24:54 +0300

    OrderSummary now doesn't show toppings header if toppings are not ordered

--- sundae-front-end/src/pages/summary/OrderSummary.jsx
+++ sundae-front-end/src/pages/summary/OrderSummary.jsx
@@ -13,19 +13,27 @@ export default function OrderSummary(props) {
     const toppingsArray = Object.entries(optionCounts.toppings); // ["M&Ms", "Gummi bears"]
     const toppingList = toppingsArray.map(topping => <li key={ topping } >{ topping }</li> )
 
-    return (
-        <div>
-            <h1>Order Summary</h1>
-            <h2>Scoops: { formatCurrency(totals.scoops) }</h2>
-            <ul>
-               { scoopList }
-            </ul>
-            <h2>Toppings: { formatCurrency(totals.toppings) }</h2>
-            <ul>
-                { toppingList }
-            </ul>
-            <h2>Total: { formatCurrency(totals.scoops + totals.toppings) }</h2>
-            <SummaryForm setOrderPhase={props.setOrderPhase} />
-        </div>
-    )
+    if (totals.toppings === 0) {
+        return (
+            <div>
+                <h1>Order Summary</h1>
+                <h2>Scoops: { formatCurrency(totals.scoops) }</h2>
+                <ul>{ scoopList }</ul>
+                <h2>Total: { formatCurrency(totals.scoops + totals.toppings) }</h2>
+                <SummaryForm setOrderPhase={props.setOrderPhase} />
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Order Summary</h1>
+                <h2>Scoops: { formatCurrency(totals.scoops) }</h2>
+                <ul>{ scoopList }</ul>
+                <h2>Toppings: { formatCurrency(totals.toppings) }</h2>
+                <ul>{ toppingList }</ul>
+                <h2>Total: { formatCurrency(totals.scoops + totals.toppings) }</h2>
+                <SummaryForm setOrderPhase={props.setOrderPhase} />
+            </div>
+        )
+    }
 }
