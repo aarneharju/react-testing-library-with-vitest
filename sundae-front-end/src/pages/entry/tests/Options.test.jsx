a6774309f57a554eb35dcb19c3eed8ea408aa85a
Aarne Harju <aarne.harju@sogeti.com>
2024-05-27 21:43:03 +0300

    Added test to Options.test.jsx - red
    Added test to Options.test.jsx to assert that the scoop total doesn't update with invalid input

--- sundae-front-end/src/pages/entry/tests/Options.test.jsx
+++ sundae-front-end/src/pages/entry/tests/Options.test.jsx
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import Options from "../Options";
 import { expect } from "vitest";
@@ -29,4 +30,36 @@ test("Displays image for each toppings option from server", async () => {
     // Confirm the alt texts of the images
     const altTexts = toppingsImages.map(element => element.alt);
     expect(altTexts).toEqual(["M&Ms topping", "Hot fudge topping", "Peanut butter cups topping"]);
+})
+
+test("The scoops total should not update if input is invalid", async () => {
+    // Setup user event
+    const user = userEvent.setup();
+
+    // Render Options
+    render(<Options optionType="scoops" />);
+    
+    // Check that total starts at 0
+    const elementScoopsTotal = screen.getByText("Scoops total", { exact: false });
+    expect(elementScoopsTotal).toHaveTextContent("$0.00");
+
+    // Add invalid input (decimal number) and check that total is 0
+    const elementScoopVanillaInput = await screen.findByRole("spinbutton", { name: /vanilla/i });
+    await user.click(elementScoopVanillaInput);
+    await user.clear(elementScoopVanillaInput);
+    await user.type(elementScoopVanillaInput, "2.5");
+    expect(elementScoopsTotal).toHaveTextContent("$0.00");
+
+    // Add invalid input (negative number) and check that total is 0
+    await user.click(elementScoopVanillaInput);
+    await user.clear(elementScoopVanillaInput);
+    await user.type(elementScoopVanillaInput, "-2");
+    expect(elementScoopsTotal).toHaveTextContent("$0.00");
+
+    // Add invalid input (number greater than 10) and check that total is 0
+    await user.click(elementScoopVanillaInput);
+    await user.clear(elementScoopVanillaInput);
+    await user.type(elementScoopVanillaInput, "20");
+    expect(elementScoopsTotal).toHaveTextContent("$0.00");
+
 })
