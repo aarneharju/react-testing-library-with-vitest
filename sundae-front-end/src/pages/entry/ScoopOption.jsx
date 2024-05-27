9ee7b87096a96a302de8bfcc5eb9a95679af9a3f
Aarne Harju <aarne.harju@sogeti.com>
2024-05-27 16:51:17 +0300

    Wrote react code to check ScoopOption input validity - green

--- sundae-front-end/src/pages/entry/ScoopOption.jsx
+++ sundae-front-end/src/pages/entry/ScoopOption.jsx
@@ -2,10 +2,30 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import { useState } from "react";
 
 export default function ScoopOption({name, imagePath}) {
     const { updateItemCount } = useOrderDetails();
-    const handleChange = (event) => updateItemCount(name, parseInt(event.target.value),"scoops");
+    const [isValid, setIsValid] = useState(true);
+
+    const handleChange = (event) => {
+        const currentValue = event.target.value;
+
+        // Make sure we're using a number, not a string to validate
+        const currentValueFloat = parseFloat(currentValue);
+
+        // Check validity
+        const valueIsValid = 
+            0 <= currentValueFloat &&
+            currentValueFloat <= 10 &&
+            Math.floor(currentValueFloat) === currentValueFloat; // Check that the value is integer, not a decimal number
+        
+        // Validate
+        setIsValid(valueIsValid);
+
+        updateItemCount(name, parseInt(event.target.value),"scoops")
+    };
+
 
     return (
         <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
@@ -23,6 +43,7 @@ export default function ScoopOption({name, imagePath}) {
                         type="number"
                         defaultValue={0}
                         onChange={handleChange}
+                        isInvalid={!isValid}
                     />
                 </Col>
             </Form.Group>
